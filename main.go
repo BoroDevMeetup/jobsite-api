4687e1bb76d0f4af88807fbb2a82fdbd00f7cc54
Rob Henley <29806+robhenley@users.noreply.github.com>
2021-03-11 12:28:02 -0600

    First pass at Slack authentication

--- main.go
+++ main.go
@@ -11,10 +11,25 @@ import (
 	"github.com/google/jsonapi"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"github.com/thanhpk/randstr"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/endpoints"
 )
 
 type jobTypeEnum string
 type experienceEnum string
+
+// Move this somewhere more appropriate
+var (
+	authConfig = &oauth2.Config{
+		RedirectURL:  os.Getenv("ORIGIN") + "/auth/callback",
+		ClientID:     os.Getenv("SLACK_CLIENT_ID"),
+		ClientSecret: os.Getenv("SLACK_CLIENT_SECRET"),
+		Scopes:       []string{"identity.basic"},
+		Endpoint:     endpoints.Slack,
+	}
+	oauthState = randstr.Hex(16)
+)
 
 const (
 	entryLevel  experienceEnum = "entryLevel"
@@ -109,6 +124,46 @@ func handleRequest() {
 	myRouter.HandleFunc("/jobs", showAllJobs).Methods("GET")
 	myRouter.HandleFunc("/jobs", testPostJobs).Methods("POST")
 
+	myRouter.HandleFunc("/auth/redirect", func(w http.ResponseWriter, r *http.Request) {
+
+		url := authConfig.AuthCodeURL(oauthState)
+
+		http.Redirect(w, r, url, http.StatusFound)
+
+	})
+
+	myRouter.HandleFunc("/auth/callback", func(w http.ResponseWriter, r *http.Request) {
+		code := r.FormValue("code")
+		state := r.FormValue("state")
+
+		if state != oauthState {
+			http.Error(w, "invalid oauth state", http.StatusInternalServerError)
+			return
+		}
+
+		token, err := authConfig.Exchange(oauth2.NoContext, code)
+		if err != nil {
+			fmt.Fprintf(w, "Error: %s", err.Error())
+			return
+		}
+
+		// teamID := token.Extra("team_id")
+		// user := token.Extra("user")
+		// fmt.Fprintf(w, "%s %v", teamID, user)
+
+		basicInfo, err := getBasicInfo(string(token.AccessToken))
+		if err != nil {
+			fmt.Fprintf(w, "Error: %s", err.Error())
+			return
+		}
+
+		err = json.NewEncoder(w).Encode(basicInfo)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+	})
+
 	apiRouter := myRouter.PathPrefix("/api/v1").Subrouter()
 	apiRouter.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
 
@@ -143,4 +198,31 @@ func defaultContentType(next http.Handler) http.Handler {
 		w.Header().Add("Content-Type", jsonapi.MediaType)
 		next.ServeHTTP(w, r)
 	})
+}
+
+func getBasicInfo(accessToken string) (d map[string]interface{}, err error) {
+
+	// NOTE: DO NOT USE https://slack.com/api/auth.test as it always
+	// responds with "invalid_auth".  I'm guessing it's a legacy endpoint
+	req, err := http.NewRequest("POST", "https://slack.com/api/users.identity", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+accessToken)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var data map[string]interface{}
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
