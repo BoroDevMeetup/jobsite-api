a8b114b339889f4bdcd8cb28a06913c2590c2ce7
Rob Henley <29806+robhenley@users.noreply.github.com>
2021-03-23 12:17:01 -0500

    Adjust initial migration to remove social_accounts  ...and switch to SERIAL

--- migrations/20210305124354_create_initial_tables.down.sql
+++ migrations/20210305124354_create_initial_tables.down.sql
@@ -1,4 +1,3 @@
 DROP TABLE companies;
 DROP TABLE jobs;
-DROP TABLE social_accounts;
 DROP TABLE users;
--- migrations/20210305124354_create_initial_tables.up.sql
+++ migrations/20210305124354_create_initial_tables.up.sql
@@ -1,6 +1,8 @@
 CREATE TABLE users
 (
-    id bigint NOT NULL,
+    id SERIAL,
+    slack_user_id character varying(50),
+    slack_access_token character varying(255),
     first_name character varying(50),
     last_name character varying(50),
     email character varying(255) NOT NULL,
@@ -11,28 +13,9 @@ CREATE TABLE users
     CONSTRAINT users_email_uniq UNIQUE (email)
 );
 
-CREATE TABLE social_accounts
-(
-    id bigint NOT NULL,
-    user_id bigint NOT NULL,
-    provider character varying(30) NOT NULL,
-    provider_user_id bigint NOT NULL,
-    access_token character varying(255) NOT NULL,
-    refresh_token character varying(255),
-    expires_in bigint,
-    created_at date,
-    updated_at date,
-    PRIMARY KEY (id),
-    CONSTRAINT social_accounts_user_id_foreign FOREIGN KEY (user_id)
-        REFERENCES users (id) MATCH SIMPLE
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-        NOT VALID
-);
-    
 CREATE TABLE jobs
 (
-    id bigint NOT NULL,
+    id SERIAL,
     user_id bigint,
     title character varying(255) NOT NULL,
     bio text,
@@ -58,8 +41,8 @@ CREATE TABLE jobs
 
 CREATE TABLE companies
 (
-    id bigint NOT NULL,
-    user_id bigint NOT NULL,
+    id SERIAL,
+    user_id bigint,
     name character varying(255),
     city character varying(255),
     state character varying(4),
